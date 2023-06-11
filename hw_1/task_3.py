2bea24cfdb39a5c7f4e91a6dd8e65ec85ec69c5a
Nikolay <pisarev82@yandex.ru>
2023-06-11 17:31:01 +0300

    Update hw_1/task_3.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- hw_1/task_3.py
+++ hw_1/task_3.py
@@ -23,7 +23,7 @@ elif num == 2:
     print("2 - единственное четное простое число. ")
 else:
     is_prime = True
-    for i in range(2, int(num**0.5)+1):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             is_prime = False
             break
